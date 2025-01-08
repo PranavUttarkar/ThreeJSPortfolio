c7ae752e7a20f0cd567e015e03bb16b4860c602a
PranavUttarkar <pranavuttarkar6@gmail.com>
2025-01-08 17:10:26 -0600

    insights extension

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const scene = new THREE.Scene();
